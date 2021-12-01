cf286fd40339c168726902e0ad11abefaa4fdfa2
jiyoungaaa <jiyoung.developer@gmail.com>
2021-12-01 15:56:55 +0900

    first

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,1 +1,11 @@
-print('git test')
+# crawling 작업
+
+# crawlling은 각자 진행하고 빨리 완성되는 코드로 연도를 나눠서 진행하겠습니다.
+# 일단 2020년 개봉작만 크롤링 해주시고 저장 형식은 csv로 하겠습니다.
+# 나머지는 연도별로 나눠서 크롤링 해서 합칠게요.
+# 컬럼명은 ['title', 'reviews']로 통일 해주세요.
+# 파일명은 reviews_0000.csv로 해주세요. 0000은 해당 연도입니다.
+# 크롤링한 데이터파일은 아래 링크로 올려주세요.
+# https://drive.google.com/drive/folders/1qW0BIwaYvog3jUNB5VD9FnkjLEtZt_3z?usp=sharing
+
+print('fork test')
